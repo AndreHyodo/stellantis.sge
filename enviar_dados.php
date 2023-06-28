3e4e025786f5bcf4501a96558025c42f6f1f80ff
André Hyodo <119762896+AndreHyodo@users.noreply.github.com>
2023-06-28 16:37:22 -0300

    Update enviar_dados.php

--- enviar_dados.php
+++ enviar_dados.php
@@ -1,11 +1,26 @@
 <?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:stellantis.database.windows.net,1433; Database = SGE", "Adm", "Stellantis@2023");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "Adm", "pwd" => "Stellantis@2023", "Database" => "SGE", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:stellantis.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
     //aqui é só um exemplo para não rodar o script abaixo sem necessidade
     //if ((isset($_POST['causal']))&&(!empty($_POST['causal']))){
     
     //porta, usuário, senha, nome data base
     //caso não consiga conectar mostra a mensagem de erro mostrada na conexão
     //$conexao = mysqli_connect("stellantis.database.windows.net", "Adm", "Stellantis@2023", "SGE") or die("Erro na conexão com banco de dados " . mysqli_error($conexao));
-
+/*
     // PHP Data Objects(PDO) Sample Code:
     $servername = "stellantis.database.windows.net";
     $username = "Adm";
@@ -53,6 +68,7 @@
     $date = "Valor date";
 
     inserirDados($id, $TestCell, $causal, $hora_inicio, $hora_final, $obs, $date);
+*/
 /*
 ?>
     
