6338eb54a22f5fe6bb77eeb5bfc6c10f91550b85
André Hyodo <119762896+AndreHyodo@users.noreply.github.com>
2023-06-28 15:52:50 -0300

    Update enviar_dados.php

--- enviar_dados.php
+++ enviar_dados.php
@@ -15,14 +15,44 @@
         print("Error connecting to SQL Server.");
         die(print_r($e));
     }
+
+    // DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
+    date_default_timezone_set('America/Sao_Paulo');
     
+    if(!empty($_POST)) {
+        try {
+        $TestCell = $_POST['TestCell']; 
+        $causal = $_POST['causal'];
+        $obs = $_POST['obs'];
+        $data = date ("Y/m/d");
+        $hora = date("H:i:s");
+        $data = date();
+        // Insert data
+        $sql_insert = "INSERT INTO " . $TestCell . " (id, TestCell, causal, hora_incio, hora_final, obs, `date`) VALUES (null, '$TestCell', '$causal', '$hora', '', '$obs', '$data')";
+    
+        $stmt = $conn->prepare($sql_insert);
+        $stmt->bindValue(2, $causal);
+        $stmt->bindValue(3, $data);
+        $stmt->bindValue(4, $hora);
+        $stmt->bindValue(5, $hora);
+        $stmt->bindValue(6, $obs);
+        $stmt->bindValue(7, $TestCell);
+        $stmt->execute();
+        }
+        catch(Exception $e) {
+        die(var_dump($e));
+        }
+        echo "<h3>Your're registered!</h3>";
+    }
+
+
+    /*
     // SQL Server Extension Sample Code:
     $connectionInfo = array("UID" => "Adm", "pwd" => "Stellantis@2023", "Database" => "SGE", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:stellantis.database.windows.net,1433";
     $conexao = sqlsrv_connect($serverName, $connectionInfo);
         
-    // DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
-    date_default_timezone_set('America/Sao_Paulo');
+    
     
     //Abaixo atribuímos os valores provenientes do formulário pelo método POST
     $TestCell = $_POST['TestCell']; 
@@ -39,7 +69,7 @@
         echo "dados inseridos com sucesso";
     }else{
         echo "erro";
-    }
+    }*/
 
     $conexao -> close();
     }
