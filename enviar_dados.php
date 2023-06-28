115e1dbcdaba60186ff65c2d446192bffba2a889
André Hyodo <119762896+AndreHyodo@users.noreply.github.com>
2023-06-28 15:31:51 -0300

    Update enviar_dados.php

--- enviar_dados.php
+++ enviar_dados.php
@@ -4,8 +4,23 @@
     
     //porta, usuário, senha, nome data base
     //caso não consiga conectar mostra a mensagem de erro mostrada na conexão
-    $conexao = mysqli_connect("stellantis.database.windows.net", "Adm", "Stellantis@2023", "SGE") or die("Erro na conexão com banco de dados " . mysqli_error($conexao));
+    //$conexao = mysqli_connect("stellantis.database.windows.net", "Adm", "Stellantis@2023", "SGE") or die("Erro na conexão com banco de dados " . mysqli_error($conexao));
 
+    // PHP Data Objects(PDO) Sample Code:
+    try {
+        $conn = new PDO("sqlsrv:server = tcp:stellantis.database.windows.net,1433; Database = SGE", "Adm", "Stellantis@2023");
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $e) {
+        print("Error connecting to SQL Server.");
+        die(print_r($e));
+    }
+    
+    // SQL Server Extension Sample Code:
+    $connectionInfo = array("UID" => "Adm", "pwd" => "Stellantis@2023", "Database" => "SGE", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+    $serverName = "tcp:stellantis.database.windows.net,1433";
+    $conn = sqlsrv_connect($serverName, $connectionInfo);
+        
     // DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
     date_default_timezone_set('America/Sao_Paulo');
     
