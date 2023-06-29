fbd9aef6b0c6c6811b22a51546e5ed19a0c6b278
André Hyodo <119762896+AndreHyodo@users.noreply.github.com>
2023-06-29 12:08:07 -0300

    Update enviar_dados.php

--- enviar_dados.php
+++ enviar_dados.php
@@ -1,4 +1,6 @@
 <?php
+/*
+
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:stellantis.database.windows.net,1433; Database = SGE", "Adm", "Stellantis@2023");
@@ -20,7 +22,7 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
     //porta, usuário, senha, nome data base
     //caso não consiga conectar mostra a mensagem de erro mostrada na conexão
     //$conexao = mysqli_connect("stellantis.database.windows.net", "Adm", "Stellantis@2023", "SGE") or die("Erro na conexão com banco de dados " . mysqli_error($conexao));
-/*
+
     // PHP Data Objects(PDO) Sample Code:
     $servername = "stellantis.database.windows.net";
     $username = "Adm";
@@ -69,63 +71,37 @@ $conn = sqlsrv_connect($serverName, $connectionInfo);
 
     inserirDados($id, $TestCell, $causal, $hora_inicio, $hora_final, $obs, $date);
 */
-/*
-?>
-    
-    if(!empty($_POST)) {
-        try {
-        $TestCell = $_POST['TestCell']; 
-        $causal = $_POST['causal'];
-        $obs = $_POST['obs'];
-        $data = date ("Y/m/d");
-        $hora = date("H:i:s");
-        $data = date();
-        // Insert data
-        $sql_insert = "INSERT INTO " . $TestCell . " (id, TestCell, causal, hora_incio, hora_final, obs, `date`) VALUES (null, '$TestCell', '$causal', '$hora', '', '$obs', '$data')";
-    
-        $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(2, $causal);
-        $stmt->bindValue(3, $data);
-        $stmt->bindValue(4, $hora);
-        $stmt->bindValue(5, $hora);
-        $stmt->bindValue(6, $obs);
-        $stmt->bindValue(7, $TestCell);
-        $stmt->execute();
-        }
-        catch(Exception $e) {
-        die(var_dump($e));
-        }
-        echo "<h3>Your're registered!</h3>";
-    }
-    */
 
-    /*
-    // SQL Server Extension Sample Code:
-    $connectionInfo = array("UID" => "Adm", "pwd" => "Stellantis@2023", "Database" => "SGE", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-    $serverName = "tcp:stellantis.database.windows.net,1433";
-    $conexao = sqlsrv_connect($serverName, $connectionInfo);
-        
-    
-    
-    //Abaixo atribuímos os valores provenientes do formulário pelo método POST
-    $TestCell = $_POST['TestCell']; 
-    $causal = $_POST['causal'];
-    $obs = $_POST['obs'];
-    $data = date ("Y/m/d");
-    $hora = date("H:i:s");
+//CONEXAO AZURE
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "stellantis.mysql.database.azure.com", "nadvxertwb", "Stellantis@2023", "sge", 3306, MYSQLI_CLIENT_SSL);
+//porta, usuário, senha, nome data base
+//caso não consiga conectar mostra a mensagem de erro mostrada na conexão
+//$conexao = mysqli_connect("stellantis.mysql.database.azure.com", "nadvxertwb", "Stellantis@2023", "sge") or die("Erro na conexão com banco de dados " . mysqli_error($conexao));
 
-    $sql = "INSERT INTO " . $TestCell . " (id, TestCell, causal, hora_incio, hora_final, obs, `date`) VALUES (null, '$TestCell', '$causal', '$hora', '', '$obs', '$data')";
-    
-    $resultado = $conexao->query($sql) or trigger_error($conexao->error);
+// DEFINE O FUSO HORARIO COMO O HORARIO DE BRASILIA
+date_default_timezone_set('America/Sao_Paulo');
 
-    if($resultado==true){
-        echo "dados inseridos com sucesso";
-    }else{
-        echo "erro";
-    }*/
+//Abaixo atribuímos os valores provenientes do formulário pelo método POST
+$TestCell = $_POST['TestCell']; 
+$causal = $_POST['causal'];
+$obs = $_POST['obs'];
+$data = date ("Y/m/d");
+$hora = date("H:i:s");
 
-    $conexao -> close();
-    }
+$sql = "INSERT INTO " . $TestCell . " (id, TestCell, causal, hora_incio, hora_final, obs, `date`) VALUES (null, '$TestCell', '$causal', '$hora', '', '$obs', '$data')";
+
+$resultado = $conexao->query($sql) or trigger_error($conexao->error);
+
+if($resultado==true){
+    echo "dados inseridos com sucesso";
+}else{
+    echo "erro";
+}
+
+$conexao -> close();
+
 ?>
 
 <link rel="stylesheet" href="./botao_return.css">
